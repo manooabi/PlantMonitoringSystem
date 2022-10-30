013fe6be25529e719a2821a5168a1c0651946076
manooabi <manoo.abhillash@gmail.com>
2022-10-30 20:29:50 +0530

    Add project files.

---
+++ PlantMonitoringSystem/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlantMonitoringSystem.Models;
+using System.Diagnostics;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PlantMonitoringSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "Z3hvicVj20nKFHVi7MZGwQHSLHkdhNU7wI5rcGaj",
+            BasePath = "https://plant-monitoring-system-app-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        };
+        IFirebaseClient client;
+
+        public IActionResult Index()
+        {
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Plants-Monitoring-App");
+            dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+            var list = new List<Plants_Monitoring_App>();
+            if (data != null)
+            {
+                foreach (var item in data)
+                {
+                    list.Add(JsonConvert.DeserializeObject<Plants_Monitoring_App>(((JProperty)item).Value.ToString()));
+                }
+            }
+
+            return View(list);
+        }
+
+    }
+}
---
+++ PlantMonitoringSystem/Models/Plants_Monitoring_App.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using System.Collections.ObjectModel;
+
+namespace PlantMonitoringSystem.Models
+{
+    public class Plants_Monitoring_App
+    {
+        public int Growth { get; set; }
+        public int Humidity { get; set; }
+        public int Light_Level { get; set; }
+
+        public int Tempurature { get; set; }
+        public virtual Collection<Soil_Moisture> sol_moisture { get; set; }
+
+    }
+    public class Soil_Moisture
+    {
+        public int LastState { get; set; }
+        public int SensorState { get; set; }
+    }
+
+}
